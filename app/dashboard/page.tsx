9f9a4cddd6d57f3bcc4ce93dfa467065c5367643
Dhruvil <dskathiriya1805@gmail.com>
2024-12-19 23:17:10 +0530

    Shadecn Ui Integration

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,9 @@
-export default function DashboardPage(){
+import { redirect } from "next/navigation";
+import { auth } from "../utils/auth"
+import { requireUser } from "../utils/hooks";
+
+export default async function DashboardPage(){
+  const session = await requireUser()
   return(
     <>
     <p>Hello</p>
