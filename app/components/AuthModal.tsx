e312f2276d1bc97e8b726156e668dbb6599791df
Dhruvil <dskathiriya1805@gmail.com>
2025-01-02 23:15:46 +0530

    UI Updates

--- app/components/AuthModal.tsx
+++ app/components/AuthModal.tsx
@@ -1,23 +1,12 @@
-import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent, DialogHeader, DialogTrigger } from "@/components/ui/dialog";
-import Image from "next/image";
-import Logo from "@/public/logo.png"
+import { Dialog } from "@/components/ui/dialog";
+
 
 export function AuthModal() {
     return (
+        <>
         <Dialog>
-
-            <DialogTrigger asChild>
-                <Button>Try For Free</Button>
-            </DialogTrigger>
-
-            <DialogContent className="sm:max-w-[360px]">
-                <DialogHeader>
-                    <Image src={Logo} alt="Logo" className="size-10" />
-                    <h4 className="text-3xl font-semibold">Chrono <span className="text-blue-500">App</span></h4>
-                </DialogHeader>
-
-            </DialogContent>
+            
         </Dialog>
+        </>
     )
 }
